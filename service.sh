142da849b24b8fb808c0a682038f336dd71138bc
VR-25 <28943176+VR-25@users.noreply.github.com>
2020-07-08 14:24:10 +0100

    v2020.7.8-beta (202007080)
    Automatic backups (Magisk, init.d and Tasker flavors)
    Major fixes & optimizations
    More comprehensive `--list`
    System data backup/restore sub-option is now D (formerly S).
    README.md has a copy of the help text.
    Updated documentation (ROM migration instructions, automatic backup configuration and more).
    Verbose is redirected to /dev/migrator.log, which can be exported to /sdcard/migrator.log.bz2 with `M -L`.
    Works in recovery environments (particularly useful for emergency backups).

--- service.sh
+++ service.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-# enable apps with Settings.Secure.ANDROID_ID (SSAID)
+# enable apps with Settings.Secure.ANDROID_ID (SSAID) and start automatic backups (if enabled)
 
 modDir=${0%/*}
-start-stop-daemon -bx $modDir/system/bin/migrator -S -- --ssaid
+start-stop-daemon -bx $modDir/system/bin/migrator -S -- --boot
 exit 0
